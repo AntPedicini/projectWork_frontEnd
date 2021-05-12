6323f0fa5be878f4f534c73b4f6fca6b03bd6cbc
AntPed <antonio.pedicini@stud.tecnicosuperiorekennedy.it>
2021-05-12 17:56:23 +0200

    completamento filtri tabelle e gestione iscrizione

--- src/app/table-presence/table-presence.component.ts
+++ src/app/table-presence/table-presence.component.ts
@@ -1,8 +1,11 @@
+import { DataSource } from '@angular/cdk/collections';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { TablePresenceDataSource, TablePresenceItem } from './table-presence-datasource';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 const EXAMPLE_DATA: TablePresenceItem[] = [
   { cod_evento: 1, nome_evento: 'GIORNATA NAZIONALE VEICOLI D EPOCA', targa: 'XXX111XXX', costo_unitario: 35.7, posti_disponibili: 1, partecipanti_iscritti: 4, partecipanti_effettivi: 3 },
@@ -45,21 +48,103 @@ export class TablePresenceComponent {
 
     postiDisponibili();
   }
+  //======================================================================
+  //Metodi per impostare i filtri nella ricerca
+  //======================================================================
+  allFilter(event: Event) {
 
+    this.dataSource.filterPredicate = (data: TablePresenceItem, filter: any) => {
+      return data.nome_evento.trim().toLowerCase().includes(filter.trim().toLowerCase()) ||
+        data.cod_evento.toString().trim().toLowerCase().includes(filter.trim().toLowerCase()) ||
+        data.targa.trim().toLowerCase().includes(filter.trim().toLowerCase())
+    };
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  cod_eventoFilter(event: Event) {
+
+    //imposta i campi di ricerca sul quale agisce il filtro
+    this.dataSource.filterPredicate = (data: TablePresenceItem, filter: any) => {
+      return data.cod_evento.toString().includes(filter);
+    };
+
+    //filtra le righe in base al criterio del filterPredicate
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  targaFilter(event: Event) {
+
+    this.dataSource.filterPredicate = (data: TablePresenceItem, filter: any) => {
+      return data.targa.trim().toLowerCase().includes(filter.trim().toLowerCase());
+    };
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+  nome_eventoFilter(event: Event) {
+
+    this.dataSource.filterPredicate = (data: TablePresenceItem, filter: any) => {
+      return data.nome_evento.trim().toLowerCase().includes(filter.trim().toLowerCase());
+    };
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  //======================================================================
+
+  //ReactiveForm
+
+  registrationForm = new FormGroup({
+    cod_evento: new FormControl(''),
+    targa: new FormControl(''),
+    partecipanti: new FormControl(''),
+    costo: new FormControl(''),
+  });
+
+  //======================================================================
+  //Metodo per calcolare in tempo reale la quota da pagare per partecipare
+  //======================================================================
+  setCosto(): string {
+    var costoTot: number = 0;
+    this.dataSource.data.forEach(element => {
+      if (element.cod_evento == this.registrationForm.value.cod_evento)
+        costoTot = element.costo_unitario * this.registrationForm.value.partecipanti;
+    });
+    //console.log(this.registrationForm.value.partecipanti)
+    return costoTot.toFixed(2); //Number.toFixed(n:number):string -> converte il numero in una stringa considerando n cifre dopo la virgola
+  }
+  //======================================================================
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.registrationForm.value);
+    alert('Submit');
+  }
 
 }
 
+//======================================================================
+//Funzione per calcolare i posti rimanenti in base alle prenotazioni
+//======================================================================
 function postiDisponibili() {
+  //per ogni elemento dei dati in ingresso prendi il numero massimo di partecipanti, sottrai la somma dei partecipanti iscritti
   var tot: number = 0;
   EXAMPLE_DATA.forEach(element => {
-    if (element.cod_evento == 1) { 
-    element.posti_disponibili = 150;
-  }
-  if (element.cod_evento == 2) { 
-    element.posti_disponibili = 62;
-  }
-  if (element.cod_evento == 3) { 
-    element.posti_disponibili = 132;
-  }
- });
+    if (element.cod_evento == 1) {
+      element.posti_disponibili = 150;
+    }
+    if (element.cod_evento == 2) {
+      element.posti_disponibili = 62;
+    }
+    if (element.cod_evento == 3) {
+      element.posti_disponibili = 132;
+    }
+  });
 }
+//======================================================================
+
