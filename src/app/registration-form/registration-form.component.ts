b34f45f982ae51d4f6fe017f1b2a1bed165ac64b
Alex Checchin <alex.checchin@stud.tecnicosuperiorekennedy.it>
2021-05-18 14:05:31 +0200

    Select Form Iscrizione

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { TablePresenceItem } from '../table-presence/table-presence-datasource';
+import { EXAMPLE_DATA } from '../table-presence/table-presence.component';
+ 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
@@ -8,6 +11,11 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 
 export class RegistrationFormComponent {
+  dataSource: MatTableDataSource<TablePresenceItem> | undefined;
+  elenco_eventi: any[] = [];
+  selected = null;
+  hasUnitNumber = false;
+
   addressForm = this.fb.group({
     nome: [null, Validators.required],
     id_evento: [null, Validators.required],
@@ -16,12 +24,15 @@ export class RegistrationFormComponent {
     iscritti: [null, Validators.required]
   });
 
-  hasUnitNumber = false;
-  elenco_nomi: String[] = [];
-  selected = null;
-
   constructor(private fb: FormBuilder) {
-    
+    this.dataSource = new MatTableDataSource(EXAMPLE_DATA);
+    var n:any=null;
+    EXAMPLE_DATA.forEach(element => {
+      if(element.cod_evento!=n){
+        this.elenco_eventi.push(element.nome_evento);
+        n=element.cod_evento;
+      }
+    });
   }
 
   onSubmit(): void {
